a22aab3fa46a40a65bc2a0432b1113ee8a1a7e27
your_usernametonythetaylor <ant.taylor2@gmail.com>
2022-06-28 10:31:18 -0400

    updating mock design

---
+++ personData.js
@@ -0,0 +1,36 @@
+const personData = [
+    {
+        'id': 1,
+        'style': 'The Jack Boot in Dark Sky',
+        'colorway': 'Black',
+        'release': 2022,
+        'brand' : 'Taft',
+         url: 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F1c%2F4a%2F1c4a13ccd296f33ab4f031f2f4db3dea0a458407.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]'
+    },
+    {
+        'id': 2,
+        'style': 'Regular Fit Cotton Shorts',
+        'colorway': 'White/Black-Red',
+        'release': 1986,
+        'brand' : 'H&M',
+        url : 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fcf%2Fd3%2Fcfd3c85d2e8114d8baabced9e476ffa3c9edccc1.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]'
+    },
+    {
+        'id': 3,
+        'style': 'Monetto',
+        'colorway': 'White / Black - Red',
+        'release': 1994,
+        'brand' : 'Zara',
+        url: 'https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/287587664_163015976213540_558677109318125441_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=1&_nc_ohc=w1kq_UaIBH0AX_E-cEV&edm=AABBvjUBAAAA&ccb=7-5&oh=00_AT_VxK1_mt7slYzTSHvIEHV1jM0Q35KGWmzyw0Opp2ykAw&oe=62C1060E&_nc_sid=83d603'
+    },
+    {
+        'id': 4,
+        'style': 'Cutout Halter Mini Dress',
+        'colorway': 'Green / Black',
+        'release': 2000,
+        'brand' : 'Forever 21',
+        url: 'https://www.forever21.com/dw/image/v2/BFKH_PRD/on/demandware.static/-/Sites-f21-master-catalog/default/dwb9187b36/1_front_750/00462904-02.jpg?sw=1000&sh=1500'
+    }
+]
+
+export default personData
--- screens/ProductScreen.jsx
+++ screens/ProductScreen.jsx
@@ -1,10 +1,101 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import { View, Text, FlatList, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import { Avatar } from 'react-native-elements';
+import personData from "../personData";
+
+const DATA = [
+    {
+        id: 1,
+        postTitle: 'Planet of Nature',
+        avatarURI:
+            'https://images.unsplash.com/photo-1559526323-cb2f2fe2591b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        imageURI:
+            'https://images.unsplash.com/photo-1482822683622-00effad5052e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        randomText:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+    },
+    {
+        id: 2,
+        postTitle: 'Lampost',
+        avatarURI:
+            'https://images.unsplash.com/photo-1559526323-cb2f2fe2591b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        imageURI:
+            'https://images.unsplash.com/photo-1482822683622-00effad5052e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        randomText:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+    }
+]
 
 export default function ProductScreen() {
-    return (
-        <View>
-            <Text>PAST LOOKS</Text>
+    const renderItem = ({ item }) => (
+        <View style={styles.container}>
+            <View style={styles.card}>
+                <Image
+                    source={{ uri: item.url }}
+                    style={styles.cardImage}
+                />
+                <View style={styles.cardHeader}>
+                    <Text category='s1' style={styles.cardTitle}>
+                        {item.brand}
+                    </Text>
+                    <TouchableOpacity
+                        onPress={() => console.log('Button pressed')}>
+                        <Image
+                            source={{ uri: item.url }}
+                            size='small'
+                            style={styles.cardAvatar}
+                        />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.cardContent}>
+                    <Text category='p2'>{item.style}</Text>
+                </View>
+            </View>
         </View>
     )
+
+    return (
+        <FlatList
+            style={styles.container}
+            data={personData}
+            renderItem={renderItem}
+            keyExtractor={personData.id}
+        />
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    card: {
+        backgroundColor: '#fff',
+        marginBottom: 25
+    },
+    cardHeader: {
+        padding: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    cardTitle: {
+        color: '#000'
+    },
+    cardImage: {
+        width: '100%',
+        height: 300,
+        resizeMode: 'contain'
+      },
+    cardAvatar: {
+        marginRight: 16,
+        width: 45, height: 45,
+        resizeMode: 'contain',
+        borderRadius: 180 / 2
+    },
+    cardContent: {
+        padding: 10,
+        borderWidth: 0.25,
+        borderColor: 'grey'
+    }
+})
